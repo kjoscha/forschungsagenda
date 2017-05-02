6e818faae077828a416b026f864a4bb9f02dd116
kjoscha <kjoscha@gmail.com>
2017-05-02 17:45:42 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
+ORGANIZATIONS = [
+  Faker::University.name,
+  Faker::University.name,
+  Faker::University.name,
+  Faker::University.name,
+]
+
 12.times do
   Participant.create(
+    sex: ['Herr', 'Frau'].sample,
+    title: ['Dr.', 'Prof.', nil].sample,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    organisation: Faker::University.name,
+    organisation: ORGANIZATIONS.sample,
+    address: Faker::Address.street_address,
+    postal_code: Faker::Address.zip,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
     email: Faker::Internet.email,
-    telephone: Faker::PhoneNumber.cell_phone
+    telephone: [nil, Faker::PhoneNumber.cell_phone].sample,
+    accepted_data_storage: true,
   )
 end
