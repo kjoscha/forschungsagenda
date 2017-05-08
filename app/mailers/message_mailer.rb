d0b9f73fecfe1e9506ef7fc8b7fe438fef816aa1
kjoscha <kjoscha@gmail.com>
2017-05-08 14:26:29 +0200

    change email text

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,5 +1,5 @@
 class MessageMailer < ActionMailer::Base
-  default from: 'info@innoz.de'
+  default from: 'forschungsagenda@nexusinstitut.de'
   layout 'mailer'
 
   def confirmation_mail(participant)
